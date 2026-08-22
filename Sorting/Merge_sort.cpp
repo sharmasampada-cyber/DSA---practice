c51caa42579eb1e4e359bfd90e9fd00959f585c4
Sampada Sharma <sharma.sampada.2207@gmail.com>
2026-08-22 18:07:58 +0530

    Update merge sort logic

--- Sorting/Merge_sort.cpp
+++ Sorting/Merge_sort.cpp
@@ -1,34 +1,34 @@
 #include <iostream>
 using namespace std;
 
-void merge(int arr[] , int low  , int high){
-    //Temporary array jismei we are adding elements after getting sorted into halves
-    int temp[1000];
-    int k=0;
-    int left=low;
-    int right =mid+1;
-    while(left<=mid && right<=high){
-        if(arr[left] < arr[right]){
-            temp[k]=arr[left]
-            left++;
-        }
-        else{
-            temp[k]=arr[right];
-            right++;
-        }
-    }
-}
+// void merge(int arr[] , int low  , int high){
+//     //Temporary array jismei we are adding elements after getting sorted into halves
+//     int temp[1000];
+//     int k=0;
+//     int left=low;
+//     int right =mid+1;
+//     while(left<=mid && right<=high){
+//         if(arr[left] < arr[right]){
+//             temp[k]=arr[left]
+//             left++;
+//         }
+//         else{
+//             temp[k]=arr[right];
+//             right++;
+//         }
+//     }
+// }
 
-void merge_sort(int arr[] , int low , int high){
-    if(low>=high) return;
-    int mid=(low+high)/2;
-    //For sorting left half
-    merge_sort(arr , low , mid);
-    //For sorting right half
-    merge_sort(arr , mid+1 , high);
-    //For soting all left and right half
-    merge_sort(arr , low , mid , high);
-}
+// void merge_sort(int arr[] , int low , int high){
+//     if(low>=high) return;
+//     int mid=(low+high)/2;
+//     //For sorting left half
+//     merge_sort(arr , low , mid);
+//     //For sorting right half
+//     merge_sort(arr , mid+1 , high);
+//     //For soting all left and right half
+//     merge_sort(arr , low , mid , high);
+// }
 
 int main(){
     int n;
