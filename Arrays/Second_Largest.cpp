c2ebcbed1f497112e7fcd60c609ba9bf87705a6b
Sampada Sharma <sharma.sampada.2207@gmail.com>
2026-08-23 21:29:16 +0530

    update largest element logic

--- Arrays/Second_Largest.cpp
+++ Arrays/Second_Largest.cpp
@@ -1,15 +1,20 @@
 #include <iostream>
 using namespace std;
 
-int largest_element(int arr[] , int n){
+int second_largest_element(int arr[] , int n){
     int largest=arr[0];
     for(int i=0 ; i<n ; i++){
         if(arr[i]>largest){
             largest=arr[i];
         }
     }
-    // cout<<"largest element is : "<<largest;    agar mai ye wla bhi call kru toh 2 baar 5 print hoga ..isliye ya toh ye call hoga ya niche function k aage cout lgega
-    return largest;
+    int second_largest=-1;
+    for(int i=0 ; i<n ; i++){
+        if(arr[i]>second_largest && arr[i]<largest){
+            second_largest=arr[i];
+        }
+    }
+    return second_largest;
 }
 
 int main(){
@@ -20,6 +25,6 @@ int main(){
     for(int i=0 ; i<n ; i++){
         cin>>arr[i];
     }
-    cout<<largest_element(arr,n);
+    cout<<second_largest_element(arr,n);
     return 0;
 }
